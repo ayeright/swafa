d45218eb29cc66abf2e3cf707ae506db0dd91e61
Scott <scottbrownlie1988@gmail.com>
2021-07-06 07:41:30 +0100

    add docstrings to online_fa_analysis stage

--- experiments/online_fa_analysis.py
+++ experiments/online_fa_analysis.py
@@ -1,30 +1,92 @@
 import os
 from pathlib import Path
-from typing import List
+from typing import List, Union
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import click
+import yaml
 
 
-def run_analysis(results: pd.DataFrame, analysis_output_dir: str):
+def run_analysis(results: pd.DataFrame, analysis_output_dir: str, metric_columns: List[str]):
+    """
+    Aggregate the experiment results and generate plots.
+
+    For each experiment, defined by the parameters observation_dim, latent_dim, spectrum_min and spectrum_max, group by
+    n_samples and compute the mean and standard error of the values in each column specified in metric_columns.
+
+    Save these results to csv files. Also, for each experiment plot the means with standard error bars and save to png
+    files.
+
+    Args:
+        results: The results of each experiment. Has len(experiments_config) * n_trials rows and the following columns:
+            - observation_dim: (int) Same as above.
+            - latent_dim: (int) Same as above.
+            - spectrum_min: (float) The lower bound of the spectrum range.
+            - spectrum_max: (float) The upper bound of the spectrum range.
+            - n_samples: (int) Same as above.
+            - covar_norm: (float) The Frobenius norm of the the true covariance matrix of the FA model.
+            - covar_distance_sklearn: (float) The Frobenius norm of the difference between the true covariance matrix
+                and the covariance matrix estimated by sklearn's `FactorAnalysis`.
+            - covar_distance_online_gradient: (float) The Frobenius norm of the difference between the true covariance
+                matrix and the covariance matrix estimated by `OnlineGradientFactorAnalysis`.
+            - covar_distance_online_em: (float) The Frobenius norm of the difference between the true covariance
+                matrix and the covariance matrix estimated by `OnlineEMFactorAnalysis`.
+            - ll_true: (float) The log-likelihood of the true FA model, given the data sampled from the model.
+            - ll_sklearn: (float) The log-likelihood of the sklearn FA model.
+            - ll_online_gradient: (float) The log-likelihood of the online gradient FA model.
+            - ll_online_em: (float) The log-likelihood of the online EM FA model.
+            - experiment: (int) The index of the experiment.
+            - trial: (int) The index of the trial within the experiment.
+        analysis_output_dir: The directory path to save the output of the analysis.
+        metric_columns: The columns which are to be aggregated.
+    """
     param_columns = ['observation_dim', 'latent_dim', 'spectrum_min', 'spectrum_max']
     group_by_column = 'n_samples'
-    metric_columns = ['covar_norm', 'covar_distance_sklearn', 'covar_distance_online_gradient',
-                      'covar_distance_online_em']
 
     param_combinations = results[param_columns].drop_duplicates()
     for _, params in param_combinations.iterrows():
-        group_means, group_standard_errors = aggregated_experiment_results(
+        group_means, group_standard_errors = aggregate_experiment_results(
             results, params, group_by_column, metric_columns,
         )
 
-        plot_and_save_results(group_means, group_standard_errors, params, analysis_output_dir)
+        params_str = params_to_string(params)
+
+        group_means.to_csv(os.path.join(
+            analysis_output_dir, f'online_fa_metric_means__{params_str}.csv'),
+        )
+
+        group_standard_errors.to_csv(os.path.join(
+            analysis_output_dir, f'online_fa_metric_standard_errors__{params_str}.csv'),
+        )
+
+        generate_and_save_error_bar_plot(
+            group_means, group_standard_errors, os.path.join(analysis_output_dir, f'online_fa__{params_str}.png'),
+        )
 
 
-def aggregated_experiment_results(results: pd.DataFrame, experiment_params: pd.Series, group_by_column: str,
-                                  metric_columns: List[str]) -> (pd.DataFrame, pd.DataFrame):
+def aggregate_experiment_results(results: pd.DataFrame, experiment_params: Union[pd.Series, dict],
+                                 group_by_column: str, metric_columns: List[str]) -> (pd.DataFrame, pd.DataFrame):
+    """
+    For the experiment with the given parameters, group by the given column and average the given metric columns over
+    all trials.
+
+    Also, compute the standard error of the mean of each group.
+
+    Args:
+        results: The results of each experiment. Each row corresponds to a single trial. Columns must contain
+            group_by_column, metric_columns and all keys in experiment_params.
+        experiment_params: The parameters of the experiment which is to be aggregated. Keys are parameter names and
+            values are parameter values.
+        group_by_column: The column to group by before aggregating the results.
+        metric_columns: The columns which are to be aggregated.
+
+    Returns:
+        group_means: The mean of each column in metric_columns for each experiment group. The number of rows is equal to
+            the number of unique values in group_by_column and the number of columns is equal to len(metric_columns).
+        group_standard_errors: The standard error of each value in group_means. Has the same shape as group_means.
+    """
     experiment_mask = np.all(
         np.hstack(
             [results[name].values.reshape(-1, 1) == value for name, value in experiment_params.items()]
@@ -38,21 +100,46 @@ def aggregated_experiment_results(results: pd.DataFrame, experiment_params: pd.S
     return group_means, group_standard_errors
 
 
-def plot_and_save_results(group_means: pd.DataFrame, group_standard_errors: pd.DataFrame, experiment_params: pd.Series,
-                          output_dir: str):
+def generate_and_save_error_bar_plot(means: pd.DataFrame, standard_errors: pd.DataFrame, png_path: str):
+    """
+    Plot the means with standard error bars.
+
+    Use a log scale for both axes.
+
+    Save the plot to the given png file.
+
+    Args:
+        means: Plot a separate line for the values in each column. Use the values in the index of the DataFrame as the
+            x-axis values.
+        standard_errors: The standard error for each value in means. Has the same shape as means.
+        png_path: The file path to save the plot as a png file.
+    """
     plt.figure(figsize=(18, 8))
-    x = group_means.index
-    for metric_name in group_means.columns:
-        plt.errorbar(x, group_means[metric_name], group_standard_errors[metric_name], label=metric_name, marker='o')
+    x = means.index
+    for metric_name in means.columns:
+        plt.errorbar(x, means[metric_name], standard_errors[metric_name], label=metric_name, marker='o')
 
     plt.xlabel('Number of samples')
     plt.xscale('log')
     plt.yscale('log')
     plt.legend()
 
-    param_str = '__'.join([f'{name}={value}' for name, value in experiment_params.items()])
-    file_path = os.path.join(output_dir, f'online_fa__{param_str}.png')
-    plt.savefig(file_path, format='png')
+    plt.savefig(png_path, format='png')
+
+
+def params_to_string(params: Union[pd.Series, dict]) -> str:
+    """
+    Convert parameter key-value pairs to a string.
+
+    E.g. dict(observation_dim=19, latent_dim=5) will be converted to 'observation_dim=19__latent_dim=5'.
+
+    Args:
+        params: Key-value parameter pairs.
+
+    Returns:
+        A string of the form 'key1=value1__key2==value2__.....'.
+    """
+    return '__'.join([f'{name}={value}' for name, value in params.items()])
 
 
 @click.command()
@@ -62,13 +149,22 @@ def main(results_input_path: str, analysis_output_dir: str):
     """
     Analyse the results from the factor analysis experiments.
 
+    Save the analysis to the given output directory.
+
     Args:
         results_input_path: The parquet file path from which to load the experiment results.
         analysis_output_dir: The directory path to save the output of the analysis.
     """
+    with open("params.yaml", 'r') as fd:
+        params = yaml.safe_load(fd)
+
     results = pd.read_parquet(results_input_path)
     Path(analysis_output_dir).mkdir(parents=True, exist_ok=True)
-    run_analysis(results, analysis_output_dir)
+    run_analysis(
+        results,
+        analysis_output_dir,
+        params['online_fa_analysis']['metric_columns'],
+    )
 
 
 if __name__ == '__main__':
