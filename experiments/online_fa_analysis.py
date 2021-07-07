eefdfd4d42a21705acafc33d89f3744bda631db7
Scott <scottbrownlie1988@gmail.com>
2021-07-07 07:44:19 +0100

    plot log-likelihood

--- experiments/online_fa_analysis.py
+++ experiments/online_fa_analysis.py
@@ -9,15 +9,17 @@ import click
 import yaml
 
 
-def run_analysis(results: pd.DataFrame, analysis_output_dir: str, metric_columns: List[str]):
+def run_analysis(results: pd.DataFrame, analysis_output_dir: str, min_samples: int):
     """
-    Aggregate the experiment results and generate plots.
+    Aggregate the experiment results and generate plots for covariance distance and log-likelihood.
 
     For each experiment, defined by the parameters observation_dim, latent_dim, spectrum_min and spectrum_max, group by
-    n_samples and compute the mean and standard error of the values in each column specified in metric_columns.
+    n_samples and compute the mean and standard error of the metrics in the experiment results. Save these statistics
+    to csv files.
 
-    Save these results to csv files. Also, for each experiment plot the means with standard error bars and save to png
-    files.
+    Also, for each experiment generate two plots, one showing the distance between the true covariance matrix and the
+    estimated covariance matrix and another showing the log-likelihood of the model, for each factor analysis (FA)
+    learning algorithm. Save these plots to png files.
 
     Args:
         results: The results of each experiment. Has len(experiments_config) * n_trials rows and the following columns:
@@ -40,15 +42,21 @@ def run_analysis(results: pd.DataFrame, analysis_output_dir: str, metric_columns
             - experiment: (int) The index of the experiment.
             - trial: (int) The index of the trial within the experiment.
         analysis_output_dir: The directory path to save the output of the analysis.
-        metric_columns: The columns which are to be aggregated.
+        min_samples: Only analyse experiments which used at least this many data samples to learn FA models.
     """
     param_columns = ['observation_dim', 'latent_dim', 'spectrum_min', 'spectrum_max']
     group_by_column = 'n_samples'
+    covar_columns = [
+        'covar_norm', 'covar_distance_sklearn', 'covar_distance_online_gradient', 'covar_distance_online_em'
+    ]
+    ll_columns = ['ll_true', 'll_sklearn', 'll_online_gradient', 'll_online_em']
+
+    results = results[results['n_samples'] >= min_samples]
 
     param_combinations = results[param_columns].drop_duplicates()
     for _, params in param_combinations.iterrows():
         group_means, group_standard_errors = aggregate_experiment_results(
-            results, params, group_by_column, metric_columns,
+            results, params, group_by_column, covar_columns + ll_columns,
         )
 
         params_str = params_to_string(params)
@@ -62,7 +70,23 @@ def run_analysis(results: pd.DataFrame, analysis_output_dir: str, metric_columns
         )
 
         generate_and_save_error_bar_plot(
-            group_means, group_standard_errors, os.path.join(analysis_output_dir, f'online_fa__{params_str}.png'),
+            group_means[covar_columns],
+            group_standard_errors[covar_columns],
+            png_path=os.path.join(analysis_output_dir, f'online_fa_covar_distance__{params_str}.png'),
+            xlabel='Number of samples',
+            ylabel='Frobenius norm',
+            xscale='log',
+            yscale='log',
+        )
+
+        generate_and_save_error_bar_plot(
+            group_means[ll_columns],
+            group_standard_errors[ll_columns],
+            png_path=os.path.join(analysis_output_dir, f'online_fa_log_likelihood__{params_str}.png'),
+            xlabel='Number of samples',
+            ylabel='Log-likelihood',
+            xscale='log',
+            yscale='linear',
         )
 
 
@@ -100,7 +124,8 @@ def aggregate_experiment_results(results: pd.DataFrame, experiment_params: Union
     return group_means, group_standard_errors
 
 
-def generate_and_save_error_bar_plot(means: pd.DataFrame, standard_errors: pd.DataFrame, png_path: str):
+def generate_and_save_error_bar_plot(means: pd.DataFrame, standard_errors: pd.DataFrame, png_path: str, xlabel: str,
+                                     ylabel: str, xscale: str = 'linear', yscale: str = 'linear'):
     """
     Plot the means with standard error bars.
 
@@ -113,15 +138,20 @@ def generate_and_save_error_bar_plot(means: pd.DataFrame, standard_errors: pd.Da
             x-axis values.
         standard_errors: The standard error for each value in means. Has the same shape as means.
         png_path: The file path to save the plot as a png file.
+        ylabel: The x-axis label.
+        ylabel: The y-axis label.
+        xscale: The type of scale to use on the x-axis.
+        yscale: The type of scale to use on the y-axis.
     """
     plt.figure(figsize=(18, 8))
     x = means.index
     for metric_name in means.columns:
         plt.errorbar(x, means[metric_name], standard_errors[metric_name], label=metric_name, marker='o')
 
-    plt.xlabel('Number of samples')
-    plt.xscale('log')
-    plt.yscale('log')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.xscale(xscale)
+    plt.yscale(yscale)
     plt.legend()
 
     plt.savefig(png_path, format='png')
@@ -159,11 +189,13 @@ def main(results_input_path: str, analysis_output_dir: str):
         params = yaml.safe_load(fd)
 
     results = pd.read_parquet(results_input_path)
+
     Path(analysis_output_dir).mkdir(parents=True, exist_ok=True)
+
     run_analysis(
         results,
         analysis_output_dir,
-        params['online_fa_analysis']['metric_columns'],
+        params['online_fa_analysis']['min_samples'],
     )
 
 
