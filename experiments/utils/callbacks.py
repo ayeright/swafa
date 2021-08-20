5e0c9495a04b705faccf1540f508d21b788e9912
Scott <scottbrownlie1988@gmail.com>
2021-08-20 08:34:00 +0100

    add BatchFactorAnalysisPosteriorEvaluationCallback callback

--- experiments/utils/callbacks.py
+++ experiments/utils/callbacks.py
@@ -1,22 +1,30 @@
-from typing import Optional
+from abc import ABC, abstractmethod
+from typing import Any, Optional, Union
 
+import numpy as np
+import torch
 from torch import Tensor
 from pytorch_lightning import Trainer, LightningModule
 from pytorch_lightning.callbacks import Callback
+from pytorch_lightning.utilities.types import STEP_OUTPUT
+from sklearn.decomposition import FactorAnalysis
 
 from swafa.custom_types import POSTERIOR_TYPE
+from swafa.utils import vectorise_weights, get_callback_epoch_range
 from experiments.utils.metrics import compute_distance_between_matrices, compute_gaussian_wasserstein_distance
 
 
-class PosteriorEvaluationCallback(Callback):
+class BasePosteriorEvaluationCallback(Callback, ABC):
     """
-    This callback measures how close the estimated posterior of a model's weights is to the true posterior.
+    This is an abstract callback which, when fully implemented, can be used to measure how close an estimated posterior
+    of a model's weights is to the true posterior.
 
-    The callback assumes that the estimated posterior is updated during training and is called at the end of each epoch.
+    Requires implementation of a method which returns the mean and covariance of the estimated posterior.
+
+    Note: This callback does not update the estimated posterior. This must be done separately.
 
     Args:
-        posterior: The estimated posterior distribution of a model's weights. It is assumed that this is updated
-            separately during training.
+        posterior: The estimated posterior distribution of a model's weights.
         true_mean: The mean of the true posterior distribution of the same model's weights. Of shape (weight_dim,).
         true_covar: The covariance matrix of the true posterior distribution of the same model's weights. Of shape
             (weight_dim, weight_dim).
@@ -32,7 +40,7 @@ class PosteriorEvaluationCallback(Callback):
             each evaluation epoch.
     """
 
-    def __init__(self, posterior: POSTERIOR_TYPE, true_mean: Tensor, true_covar: Tensor, eval_epoch_frequency: int = 1):
+    def __init__(self, posterior: Any, true_mean: Tensor, true_covar: Tensor, eval_epoch_frequency: int = 1):
         self.posterior = posterior
         self.true_mean = true_mean
         self.true_covar = true_covar
@@ -60,8 +68,7 @@ class PosteriorEvaluationCallback(Callback):
             unused: Only present to match the signature of the original method.
         """
         if (self.eval_epoch_frequency > 0) & (trainer.current_epoch % self.eval_epoch_frequency == 0):
-            mean = self.posterior.get_mean()
-            covar = self.posterior.get_covariance()
+            mean, covar = self.get_mean_and_covariance()
 
             self.eval_epochs.append(trainer.current_epoch)
             self.distances_from_mean.append(
@@ -73,3 +80,139 @@ class PosteriorEvaluationCallback(Callback):
             self.wasserstein_distances.append(
                 compute_gaussian_wasserstein_distance(self.true_mean, self.true_covar, mean, covar)
             )
+
+    @abstractmethod
+    def get_mean_and_covariance(self) -> (Tensor, Tensor):
+        """
+        Get the mean and covariance of the estimated posterior of the model's weights.
+
+        Returns:
+            mean: The mean of the posterior. Of shape (weight_dim,)
+            covar: The covariance of the posterior. Of shape (weight_dim, weight_dim).
+        """
+        ...
+
+
+class OnlinePosteriorEvaluationCallback(BasePosteriorEvaluationCallback):
+    """
+    This callback measures how close the estimated posterior of a model's weights is to the true posterior.
+
+    The callback assumes that the estimated posterior is updated online during training and is called at the end of each
+    epoch.
+
+    Args:
+        posterior: The estimated posterior distribution of a model's weights.
+        true_mean: The mean of the true posterior distribution of the same model's weights. Of shape (weight_dim,).
+        true_covar: The covariance matrix of the true posterior distribution of the same model's weights. Of shape
+            (weight_dim, weight_dim).
+        eval_epoch_frequency: The number of epochs between each evaluation of the posterior.
+    """
+
+    def __init__(self, posterior: POSTERIOR_TYPE, true_mean: Tensor, true_covar: Tensor, eval_epoch_frequency: int = 1):
+        super().__init__(posterior, true_mean, true_covar, eval_epoch_frequency=eval_epoch_frequency)
+
+    def get_mean_and_covariance(self) -> (Tensor, Tensor):
+        """
+        Get the mean and covariance of the estimated posterior of the model's weights.
+
+        Returns:
+            mean: The mean of the posterior. Of shape (weight_dim,)
+            covar: The covariance of the posterior. Of shape (weight_dim, weight_dim).
+        """
+        return self.posterior.get_mean(), self.posterior.get_covariance()
+
+
+class BatchFactorAnalysisPosteriorEvaluationCallback(BasePosteriorEvaluationCallback):
+    """
+    This callback measures how close the estimated posterior of a model's weights is to the true posterior.
+
+    The posterior is learned via a batch factor analysis (FA) algorithm (randomised SVD). The model's weights are
+    collected after each batch training step. Before each evaluation of the posterior, the batch FA algorithm is fit to
+    the weight vectors which have been collected up to that point.
+
+    Args:
+        latent_dim: The latent dimension of the batch FA model.
+        true_mean: The mean of the true posterior distribution of the same model's weights. Of shape (weight_dim,).
+        true_covar: The covariance matrix of the true posterior distribution of the same model's weights. Of shape
+            (weight_dim, weight_dim).
+        collect_epoch_start: The training epoch on which to start collecting weight vectors. Integer indexing starts
+            from 1. Can also specify a float between 0 and 1, which corresponds to the fraction of total epochs which
+            should pass before starting to collect weight vectors.
+        eval_epoch_frequency: The number of epochs between each evaluation of the posterior.
+        random_seed: The random seed used when fitting the FA model.
+
+    Attributes:
+        first_collect_epoch: The first epoch on which weight vectors will be collected.
+        last_collect_epoch: The last epoch on which weight vectors will be collected.
+        weight_iterates: A list of arrays, which are the weight vectors collected during training.
+    """
+
+    def __init__(self, latent_dim: int, true_mean: Tensor, true_covar: Tensor,
+                 collect_epoch_start: Union[int, float] = 1, eval_epoch_frequency: int = 1, random_seed: int = 0):
+        error_msg = f"collect_epoch_start should be a positive integer or a float between 0 and 1, " \
+                    f"not {collect_epoch_start}"
+        if isinstance(collect_epoch_start, int) and collect_epoch_start < 1:
+            raise ValueError(error_msg)
+        if isinstance(collect_epoch_start, float) and not (0 <= collect_epoch_start <= 1):
+            raise ValueError(error_msg)
+
+        posterior = FactorAnalysis(n_components=latent_dim, svd_method='randomized', random_state=random_seed)
+        super().__init__(posterior, true_mean, true_covar, eval_epoch_frequency=eval_epoch_frequency)
+
+        self._collect_epoch_start = collect_epoch_start
+
+        self.first_collect_epoch = None
+        self.last_collect_epoch = None
+        self.weight_iterates = []
+
+    def on_fit_start(self, trainer: Trainer, pl_module: LightningModule):
+        """
+        Called when fit begins.
+
+        Initialise the range of epochs on which weight vectors will be collected
+
+        Args:
+            trainer: A PyTorch Lightning Trainer which trains the model.
+            pl_module: The model being trained.
+        """
+        self.first_collect_epoch, self.last_collect_epoch = get_callback_epoch_range(
+            trainer, epoch_start=self._collect_epoch_start,
+        )
+
+    def on_train_batch_end(self, trainer: Trainer, pl_module: LightningModule, outputs: STEP_OUTPUT, batch: Any,
+                           batch_idx: int, dataloader_idx: int):
+        """
+        Called when the train batch ends.
+
+        Collect the model's current weight vector.
+
+        Args:
+            trainer: A PyTorch Lightning Trainer which trains the model.
+            pl_module: The model being trained.
+            outputs: Not used.
+            batch: Not used.
+            batch_idx: Not used.
+            dataloader_idx: Not used.
+        """
+        if self.first_collect_epoch <= trainer.current_epoch <= self.last_collect_epoch:
+            self.weight_iterates.append(vectorise_weights(pl_module).detach().cpu().numpy())
+
+    def get_mean_and_covariance(self) -> (Tensor, Tensor):
+        """
+        Fit a batch FA algorithm to the weight vectors and return the mean and covariance of the FA model.
+
+        If no weight vectors have been collected yet, return zero tensors.
+
+        Returns:
+            mean: The mean of the posterior. Of shape (weight_dim,)
+            covar: The covariance of the posterior. Of shape (weight_dim, weight_dim).
+        """
+        if len(self.weight_iterates) == 0:
+            return torch.zeros_like(self.true_mean), torch.zeros_like(self.true_covar)
+
+        W = np.vstack(self.weight_iterates)
+        self.posterior.fit(W)
+        mean = torch.from_numpy(self.posterior.mean_).float()
+        covar = torch.from_numpy(self.posterior.get_covariance()).float()
+
+        return mean, covar
--- tests/test_experiments/test_utils/test_callbacks.py
+++ tests/test_experiments/test_utils/test_callbacks.py
@@ -1,21 +1,28 @@
+from typing import List
+
 import numpy as np
 import pytest
 import torch
 from torch import Tensor
 from torch.utils.data import TensorDataset, DataLoader
-from pytorch_lightning import Trainer
+from pytorch_lightning import Trainer, LightningModule
+from pytorch_lightning.callbacks import Callback
 
 from swafa.models import FeedForwardNet
 from swafa.fa import OnlineGradientFactorAnalysis
 from swafa.posterior import ModelPosterior
-from experiments.utils.callbacks import PosteriorEvaluationCallback
+from swafa.utils import get_weight_dimension
+from experiments.utils.callbacks import (
+    OnlinePosteriorEvaluationCallback,
+    BatchFactorAnalysisPosteriorEvaluationCallback,
+)
 from experiments.utils.metrics import (
     compute_distance_between_matrices,
     compute_gaussian_wasserstein_distance,
 )
 
 
-class TestPosteriorEvaluationCallback:
+class TestBasePosteriorEvaluationCallback:
 
     @pytest.mark.parametrize(
         "n_epochs, eval_epoch_frequency",
@@ -27,7 +34,15 @@ class TestPosteriorEvaluationCallback:
         ]
     )
     def test_eval_epochs(self, n_epochs, eval_epoch_frequency):
-        callback, _, _, _ = _fit_model_with_callback(n_epochs, eval_epoch_frequency)
+        n_samples = 20
+        input_dim = 4
+        hidden_dims = [8, 8]
+
+        callback, true_mean, true_covar, model_posterior = _init_model_with_online_posterior_evaluation_callback(
+            input_dim, hidden_dims, eval_epoch_frequency,
+        )
+        _fit_model_with_callback(model_posterior.model, callback, n_samples, input_dim, n_epochs)
+
         assert callback.eval_epochs == [i for i in range(n_epochs) if i % eval_epoch_frequency == 0]
 
     @pytest.mark.parametrize(
@@ -40,9 +55,18 @@ class TestPosteriorEvaluationCallback:
         ]
     )
     def test_distances_from_mean(self, n_epochs, eval_epoch_frequency):
-        callback, true_mean, _, posterior = _fit_model_with_callback(n_epochs, eval_epoch_frequency)
+        n_samples = 20
+        input_dim = 4
+        hidden_dims = [8, 8]
+
+        callback, true_mean, true_covar, model_posterior = _init_model_with_online_posterior_evaluation_callback(
+            input_dim, hidden_dims, eval_epoch_frequency,
+        )
+        _fit_model_with_callback(model_posterior.model, callback, n_samples, input_dim, n_epochs)
+
+        actual_mean, actual_covar = callback.get_mean_and_covariance()
         assert callback.distances_from_mean == [
-            compute_distance_between_matrices(true_mean, posterior.get_mean())
+            compute_distance_between_matrices(true_mean, actual_mean)
             for i in range(n_epochs) if i % eval_epoch_frequency == 0
         ]
 
@@ -56,9 +80,18 @@ class TestPosteriorEvaluationCallback:
         ]
     )
     def test_distances_from_covar(self, n_epochs, eval_epoch_frequency):
-        callback, _, true_covar, posterior = _fit_model_with_callback(n_epochs, eval_epoch_frequency)
+        n_samples = 20
+        input_dim = 4
+        hidden_dims = [8, 8]
+
+        callback, true_mean, true_covar, model_posterior = _init_model_with_online_posterior_evaluation_callback(
+            input_dim, hidden_dims, eval_epoch_frequency,
+        )
+        _fit_model_with_callback(model_posterior.model, callback, n_samples, input_dim, n_epochs)
+
+        actual_mean, actual_covar = callback.get_mean_and_covariance()
         assert callback.distances_from_covar == [
-            compute_distance_between_matrices(true_covar, posterior.get_covariance())
+            compute_distance_between_matrices(true_covar, actual_covar)
             for i in range(n_epochs) if i % eval_epoch_frequency == 0
         ]
 
@@ -72,13 +105,20 @@ class TestPosteriorEvaluationCallback:
         ]
     )
     def test_wasserstein_distances(self, n_epochs, eval_epoch_frequency):
-        callback, true_mean, true_covar, posterior = _fit_model_with_callback(n_epochs, eval_epoch_frequency)
+        n_samples = 20
+        input_dim = 4
+        hidden_dims = [8, 8]
+
+        callback, true_mean, true_covar, model_posterior = _init_model_with_online_posterior_evaluation_callback(
+            input_dim, hidden_dims, eval_epoch_frequency,
+        )
+        _fit_model_with_callback(model_posterior.model, callback, n_samples, input_dim, n_epochs)
+
         actual_distances = [x if not np.isnan(x) else -1 for x in callback.wasserstein_distances]
 
+        actual_mean, actual_covar = callback.get_mean_and_covariance()
         expected_distances = [
-            compute_gaussian_wasserstein_distance(
-                true_mean, true_covar, posterior.get_mean(), posterior.get_covariance()
-            )
+            compute_gaussian_wasserstein_distance(true_mean, true_covar, actual_mean, actual_covar)
             for i in range(n_epochs) if i % eval_epoch_frequency == 0
         ]
         expected_distances = [x if not np.isnan(x) else -1 for x in expected_distances]
@@ -86,10 +126,83 @@ class TestPosteriorEvaluationCallback:
         assert actual_distances == expected_distances
 
 
-def _fit_model_with_callback(n_epochs: int, eval_epoch_frequency: int) -> (PosteriorEvaluationCallback, Tensor, Tensor):
-    n_samples = 50
-    input_dim = 4
-    hidden_dims = [8, 8]
+class TestOnlinePosteriorEvaluationCallback:
+
+    @pytest.mark.parametrize("input_dim", [2, 4])
+    @pytest.mark.parametrize("hidden_dims", [[2], [4, 4]])
+    def test_get_mean_and_covariance(self, input_dim, hidden_dims):
+        eval_epoch_frequency = 1
+
+        callback, true_mean, true_covar, model_posterior = _init_model_with_online_posterior_evaluation_callback(
+            input_dim, hidden_dims, eval_epoch_frequency,
+        )
+
+        actual_mean, actual_covar = callback.get_mean_and_covariance()
+
+        assert isinstance(actual_mean, Tensor)
+        assert isinstance(actual_covar, Tensor)
+        assert actual_mean.shape == true_mean.shape
+        assert actual_covar.shape == actual_covar.shape
+        assert (torch.diag(actual_covar) > 0).all()
+
+
+class TestBatchFactorAnalysisPosteriorEvaluationCallback:
+
+    @pytest.mark.parametrize(
+        "n_samples, batch_size, n_epochs, collect_epoch_start, eval_epoch_frequency, expected_n_weight_iterates",
+        [
+            (32, 4, 5, 1, 1, int(32 / 4) * 5),
+            (32, 4, 5, 3, 2, int(32 / 4) * (5 - 2)),
+            (32, 4, 8, 0.5, 1, int(32 / 4) * (8 - 3)),
+            (32, 4, 9, 0.5, 2, int(32 / 4) * (9 - 3)),
+        ]
+    )
+    def test_n_weight_iterates(self, n_samples, batch_size, n_epochs, collect_epoch_start, eval_epoch_frequency,
+                               expected_n_weight_iterates):
+        input_dim = 4
+        hidden_dims = [8, 8]
+
+        callback, true_mean, true_covar, model = _init_model_with_batch_factor_analysis_evaluation_callback(
+            input_dim, hidden_dims, collect_epoch_start, eval_epoch_frequency,
+        )
+        _fit_model_with_callback(model, callback, n_samples, input_dim, n_epochs)
+
+        assert len(callback.weight_iterates) == expected_n_weight_iterates
+
+    @pytest.mark.parametrize(
+        "input_dim, hidden_dims, n_epochs, collect_epoch_start, eval_epoch_frequency",
+        [
+            (2, [2], 2, 1, 1),
+            (4, [4, 4], 2, 1, 1),
+            (2, [2], 2, 3, 1),
+            (4, [4, 4], 2, 3, 1),
+        ]
+    )
+    def test_get_mean_and_covariance(self, input_dim, hidden_dims, n_epochs, collect_epoch_start, eval_epoch_frequency):
+        n_samples = 20
+
+        callback, true_mean, true_covar, model = _init_model_with_batch_factor_analysis_evaluation_callback(
+            input_dim, hidden_dims, collect_epoch_start, eval_epoch_frequency,
+        )
+        _fit_model_with_callback(model, callback, n_samples, input_dim, n_epochs)
+
+        actual_mean, actual_covar = callback.get_mean_and_covariance()
+
+        assert isinstance(actual_mean, Tensor)
+        assert isinstance(actual_covar, Tensor)
+        assert actual_mean.shape == true_mean.shape
+        assert actual_covar.shape == actual_covar.shape
+
+        if collect_epoch_start <= eval_epoch_frequency:
+            assert (torch.diag(actual_covar) > 0).all()
+        else:
+            assert torch.isclose(actual_mean, torch.zeros_like(actual_mean)).all()
+            assert torch.isclose(actual_covar, torch.zeros_like(actual_covar)).all()
+
+
+def _init_model_with_online_posterior_evaluation_callback(
+        input_dim: int, hidden_dims: List[int], eval_epoch_frequency: int,
+) -> (OnlinePosteriorEvaluationCallback, Tensor, Tensor, ModelPosterior):
     net = FeedForwardNet(input_dim, hidden_dims)
 
     model_posterior = ModelPosterior(
@@ -102,18 +215,40 @@ def _fit_model_with_callback(n_epochs: int, eval_epoch_frequency: int) -> (Poste
     true_mean = torch.randn(weight_dim)
     true_covar = torch.rand(weight_dim, weight_dim)
 
-    callback = PosteriorEvaluationCallback(
+    callback = OnlinePosteriorEvaluationCallback(
         posterior=model_posterior.weight_posterior,
         true_mean=true_mean,
         true_covar=true_covar,
         eval_epoch_frequency=eval_epoch_frequency
     )
 
+    return callback, true_mean, true_covar, model_posterior
+
+
+def _init_model_with_batch_factor_analysis_evaluation_callback(
+        input_dim: int, hidden_dims: List[int], collect_epoch_start: int, eval_epoch_frequency: int,
+) -> (OnlinePosteriorEvaluationCallback, Tensor, Tensor, LightningModule):
+    net = FeedForwardNet(input_dim, hidden_dims)
+
+    weight_dim = get_weight_dimension(net)
+    true_mean = torch.randn(weight_dim)
+    true_covar = torch.rand(weight_dim, weight_dim)
+
+    callback = BatchFactorAnalysisPosteriorEvaluationCallback(
+        latent_dim=2,
+        true_mean=true_mean,
+        true_covar=true_covar,
+        collect_epoch_start=collect_epoch_start,
+        eval_epoch_frequency=eval_epoch_frequency
+    )
+
+    return callback, true_mean, true_covar, net
+
+
+def _fit_model_with_callback(model: LightningModule, callback: Callback, n_samples: int, input_dim: int, n_epochs: int):
     trainer = Trainer(max_epochs=n_epochs, callbacks=[callback])
 
     dataset = TensorDataset(torch.randn(n_samples, input_dim), torch.randn(n_samples))
     dataloader = DataLoader(dataset, batch_size=4, drop_last=True)
 
-    trainer.fit(model_posterior.model, train_dataloader=dataloader)
-
-    return callback, true_mean, true_covar, model_posterior.weight_posterior
+    trainer.fit(model, train_dataloader=dataloader)
