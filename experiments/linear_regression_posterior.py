775f29ceecca1ef52116e3d301b588b27ecfbb3a
Scott <scottbrownlie1988@gmail.com>
2021-09-26 12:55:52 +0100

    change output typing

--- experiments/linear_regression_posterior.py
+++ experiments/linear_regression_posterior.py
@@ -26,7 +26,7 @@ from experiments.utils.factory import OPTIMISER_FACTORY
 
 
 def run_all_experiments(
-        datasets: List[pd.DataFrame],
+        datasets: pd.DataFrame,
         dataset_labels: List[str],
         min_latent_dim: int,
         max_latent_dim: int,
